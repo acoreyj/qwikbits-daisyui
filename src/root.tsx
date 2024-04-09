63e8ee5f6733da0b76d4673ee24120459085a236
Corey Jepperson <corey.jepperson@gmail.com>
2024-04-08 21:04:08 -0500

    cal sans

--- src/root.tsx
+++ src/root.tsx
@@ -5,7 +5,7 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-
+import "cal-sans";
 import "./global.css";
 
 export default component$(() => {
